0270341ae52b4fe04cdbd1a3bd2d56e16ade7769
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-09 00:08:45 +0100

    Add NekoConst

---
+++ SKNIBot/Core/Const/HttpNekoConst.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SKNIBot.Core.Const
+{
+    public class HttpNekoConst
+    {
+        /// <summary>
+        /// Lista kodów, które może zwrócić strona
+        /// </summary>
+    }
+}
