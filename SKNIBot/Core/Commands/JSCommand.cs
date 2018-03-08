c1e6a382b64ff1df5349179e3e03697ead0f6302
Pawel <dziedzicpm@gmail.com>
2018-03-08 23:05:03 +0100

    Just JS

---
+++ SKNIBot/Core/Commands/JSCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Net;
+
+namespace SKNIBot.Core.Commands {
+    [CommandsGroup]
+    public class JSCommand {
+        [Command("js")]
+        [Description("Just Javascript.")]
+        //[Aliases("pies", "dog")]
+        public async Task JS(CommandContext ctx) {
+          
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync($"JavaScript ssie");
+        }
+    }
+}
