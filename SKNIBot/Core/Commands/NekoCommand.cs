89898dec8ad5748fcba0cdbe33c1efc619c71b51
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-08 23:34:33 +0100

    Cats as picture

--- SKNIBot/Core/Commands/NekoCommand.cs
+++ SKNIBot/Core/Commands/NekoCommand.cs
@@ -4,6 +4,7 @@ using DSharpPlus.CommandsNext.Attributes;
 using System.Net;
 using SKNIBot.Core.Settings;
 using Newtonsoft.Json;
+using System.IO;
 
 namespace SKNIBot.Core.Commands
 {
@@ -14,12 +15,14 @@ namespace SKNIBot.Core.Commands
         [Description("Display some cute cat.")]
         [Aliases("kot", "cat")]
         public async Task Neko(CommandContext ctx)
-        {
+        { 
             var client = new WebClient();
             var cat = client.DownloadString("http://random.cat/meow");
-            NekoContainer nekoContainer = JsonConvert.DeserializeObject<NekoContainer>(cat);
+            NekoContainer nekoContainer = JsonConvert.DeserializeObject<NekoContainer>(cat);            
+            var catPicture = client.DownloadData(nekoContainer.File);
+            Stream stream = new MemoryStream(catPicture);
             await ctx.TriggerTypingAsync();
-            await ctx.RespondAsync($"" + nekoContainer.File);
+            await ctx.RespondWithFileAsync(stream, "neko.jpg");
         }
     }
 }
