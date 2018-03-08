5b2078085c72a34ab52b8d8de8a669e67b0c192a
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-09 00:09:03 +0100

    Change HttpCode command

--- SKNIBot/Core/Commands/HttpNekoCommand.cs
+++ SKNIBot/Core/Commands/HttpNekoCommand.cs
@@ -4,6 +4,8 @@ using DSharpPlus.CommandsNext.Attributes;
 using System.Net;
 using SKNIBot.Core.Settings;
 using Newtonsoft.Json;
+using SKNIBot.Core.Const;
+using System.IO;
 
 namespace SKNIBot.Core.Commands
 {
@@ -15,15 +17,38 @@ namespace SKNIBot.Core.Commands
         [Aliases("httpcode")]
         public async Task HttpNeko(CommandContext ctx)
         {
+            //Jeżeli długość jest jeden nie podano kodu
             if(ctx.Message.Content.Split(' ').Length == 1)
             {
                 await ctx.TriggerTypingAsync();
-                await ctx.RespondAsync($"Składnia to '!httpneko {{kod}}' lub '!httpcode {{kod}}'");
+                await ctx.RespondAsync($"Składnia to '!httpneko {{kod}}' lub '!httpcode {{kod}}'. Napisz '!httpneko kody' lub '!httpcode kody' aby otrzymać listę dostępnych kodów.");
             }
+            //Jeżeli podano kod sprawdzamy czy można otrzymać takiego kota
+            else if(HttpNekoConst.Codes.Contains(ctx.Message.Content.Split(' ')[1]))
+            {
+                var client = new WebClient();
+                var httpCatPicture = client.DownloadData("https://http.cat/" + ctx.Message.Content.Split(' ')[1]);
+                Stream stream = new MemoryStream(httpCatPicture);
+                await ctx.TriggerTypingAsync();
+                await ctx.RespondWithFileAsync(stream, "httpneko.jpg");
+            }
+            //Jeżeli użytkownik prosi o kody kotów podajemy je
+            else if(ctx.Message.Content.Split(' ')[1] == "kody")
+            {
+                string availableCodes = "";
+                foreach(string code in HttpNekoConst.Codes)
+                {
+                    availableCodes += code;
+                    availableCodes += " ";
+                }
+                await ctx.TriggerTypingAsync();
+                await ctx.RespondAsync($"Kody: " + availableCodes);
+            }
+            //Jeżeli nie można otrzymać kota informujemy o błędnym kodzie
             else
             {
                 await ctx.TriggerTypingAsync();
-                await ctx.RespondAsync($"https://http.cat/" + ctx.Message.Content.Split(' ')[1]);
+                await ctx.RespondAsync($"Błędny kod http. Napisz '!httpneko kody' lub '!httpcode kody' aby otrzymać listę dostępnych kodów.");
             }
             
         }
