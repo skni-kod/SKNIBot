a8c3dca781137ee5af80e6201c1df7e2e34fac3d
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-08 22:23:12 +0100

    Add inu command

---
+++ SKNIBot/Core/Commands/InuCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SKNIBot.Core.Commands
+{
+    [CommandsGroup]
+    public class InuCommand
+    {
+        [Command("inu")]
+        [Description("Display some cute dogs.")]
+        [Aliases("pies", "dog")]
+        public async Task Inu(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync($"Cute dog.jpg");
+        }
+    }
+}
