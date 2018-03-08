028e4b24828ff7a0c10c501c07f9a098583beb76
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-08 23:18:57 +0100

    Add HttpNeko

---
+++ SKNIBot/Core/Commands/HttpNekoCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Net;
+using SKNIBot.Core.Settings;
+using Newtonsoft.Json;
+
+namespace SKNIBot.Core.Commands
+{
+    [CommandsGroup]
+    public class HttpNekoCommand
+    {
+        [Command("httpneko")]
+        [Description("Display http codes using cats.")]
+        [Aliases("httpcode")]
+        public async Task HttpNeko(CommandContext ctx)
+        {
+            if(ctx.Message.Content.Split(' ').Length == 1)
+            {
+                await ctx.TriggerTypingAsync();
+                await ctx.RespondAsync($"Składnia to '!httpneko *kod*' lub '!httpcode *kod*'");
+            }
+            else
+            {
+                await ctx.TriggerTypingAsync();
+                await ctx.RespondAsync($"https://http.cat/" + ctx.Message.Content.Split(' ')[1]);
+            }
+            
+        }
+    }
+}
