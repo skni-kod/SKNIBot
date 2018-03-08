1396ee13cdcf5afd8ef5a15bca42520c3f268f40
tearth <Tearth@users.noreply.github.com>
2018-03-08 18:58:05 +0100

    Add settings classes.

---
+++ SKNIBot/Core/Settings/SettingsContainer.cs
@@ -0,0 +1,8 @@
+﻿namespace SKNIBot.Core.Settings
+{
+    public class SettingsContainer
+    {
+        public string Token { get; set; }
+        public string Prefix { get; set; }
+    }
+}
---
+++ SKNIBot/Core/Settings/SettingsLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace SKNIBot.Core.Settings
+{
+    public static class SettingsLoader
+    {
+        public static SettingsContainer Container { get; private set; }
+
+        private static string _settingsFile = "settings.json";
+
+        static SettingsLoader()
+        {
+            Load();
+        }
+
+        private static void Load()
+        {
+            using (var settingsFile = new StreamReader(_settingsFile))
+            {
+                Container = JsonConvert.DeserializeObject<SettingsContainer>(settingsFile.ReadToEnd());
+            }
+        }
+    }
+}
