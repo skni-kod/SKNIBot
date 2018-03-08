c93dccaeda68645b8a237bbd5e63ea7b6b2b27e1
tearth <Tearth@users.noreply.github.com>
2018-03-08 21:41:55 +0100

    Add Neko command.

---
+++ SKNIBot/Core/Commands/NekoCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SKNIBot.Core.Commands
+{
+    public class NekoCommand
+    {
+        [Command("neko")]
+        [Description("Display some cute cat.")]
+        [Aliases("kot", "cat")]
+        public async Task Neko(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync($"Cute cat.jpg");
+        }
+    }
+}
