c8903a8997d7ac61988ebd106bb9cad62c76076b
tearth <Tearth@users.noreply.github.com>
2018-03-08 23:57:56 +0100

    Add MikuAllStars command.

---
+++ SKNIBot/Core/Commands/MikuAllStarsCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SKNIBot.Core.Commands
+{
+    [CommandsGroup]
+    public class MikuAllStarsCommand
+    {
+        [Command("mikuallstars")]
+        [Description("Sing!")]
+        public async Task MikuAllStars(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync("https://www.youtube.com/watch?v=vLEs8iOFkAU"); ;
+        }
+    }
+}
