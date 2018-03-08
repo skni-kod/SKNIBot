5c5434dd4c43d964477274d38f1d3231064c6160
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-08 23:00:01 +0100

    Add lenny face

---
+++ SKNIBot/Core/Commands/LennyCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Net;
+using Newtonsoft.Json;
+using SKNIBot.Core.Settings;
+
+namespace SKNIBot.Core.Commands
+{
+    [CommandsGroup]
+    public class LennyCommand
+    {
+        [Command("lenny")]
+        [Description("Display lenny.")]
+        public async Task Lenny(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync($"( ͡° ͜ʖ ͡°)");
+        }
+    }
+}
