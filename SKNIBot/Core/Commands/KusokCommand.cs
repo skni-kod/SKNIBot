854ef1bf5977fdf97ef4c5e8e0f60b0468ef9cdb
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-09 23:03:56 +0100

    Add Kusok command

---
+++ SKNIBot/Core/Commands/KusokCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SKNIBot.Core.Commands
+{
+    [CommandsGroup]
+    public class KusokCommand
+    {
+        [Command("kusok")]
+        [Description("Kanał Kusoka.")]
+        public async Task Kusok(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync("https://www.youtube.com/channel/UC6qnBvPmCZEOHOVaaXPLZhg");
+        }
+    }
+}
