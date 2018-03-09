f8e908843f0f1222d6c20058bbeb3054730dc6a8
tearth <Tearth@users.noreply.github.com>
2018-03-09 13:29:51 +0100

    Add 'upcoming' parameter to spacex command.

--- SKNIBot/Core/Commands/SpaceX/SpaceXCommand.cs
+++ SKNIBot/Core/Commands/SpaceX/SpaceXCommand.cs
@@ -26,6 +26,8 @@ namespace SKNIBot.Core.Commands.SpaceX
         [Description("TODO.")]
         public async Task SpaceX(CommandContext ctx)
         {
+            await ctx.TriggerTypingAsync();
+
             var parameters = ctx.Message.Content.Split(' ').Skip(1).ToList();
             if (parameters.Count > 0)
             {
@@ -34,6 +36,12 @@ namespace SKNIBot.Core.Commands.SpaceX
                     case "random":
                     {
                         await SendRandomFlight(ctx);
+                        break;
+                    }
+
+                    case "upcoming":
+                    {
+                        await SendUpcomingFlight(ctx);
                         break;
                     }
                 }
@@ -59,13 +67,48 @@ namespace SKNIBot.Core.Commands.SpaceX
             {
                 responseBuilder.Append($"{payload.Payload_ID} ({payload.Payload_Mass_KG} kg) to {payload.Orbit} orbit\r\n");
             }
-            responseBuilder.Append($"\r\n");
 
-            responseBuilder.Append($"{flightToDisplay.Details}\r\n");
+            if (flightToDisplay.Details != null)
+            {
+                responseBuilder.Append($"\r\n");
+                responseBuilder.Append($"{flightToDisplay.Details}\r\n");
+            }
+
             responseBuilder.Append($"\r\n");
             responseBuilder.Append($"{flightToDisplay.Links.Video_Link}\r\n");
 
-            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync(responseBuilder.ToString());
+        }
+
+        private async Task SendUpcomingFlight(CommandContext ctx)
+        {
+            var client = new WebClient();
+
+            var upcomingLaunches = client.DownloadString("https://api.spacexdata.com/v2/launches/upcoming");
+            var flights = JsonConvert.DeserializeObject<List<FlightData>>(upcomingLaunches);
+            var flightToDisplay = flights[0];
+
+            var responseBuilder = new StringBuilder();
+            responseBuilder.Append($"**{flightToDisplay.Rocket.Rocket_Name} {flightToDisplay.Rocket.Rocket_Type} ({flightToDisplay.Launch_Date_UTC})**\r\n");
+            responseBuilder.Append($"\r\n");
+
+            responseBuilder.Append($"**Payloads:**\r\n");
+            foreach (var payload in flightToDisplay.Rocket.Second_Stage.Payloads)
+            {
+                responseBuilder.Append($"{payload.Payload_ID} ({payload.Payload_Mass_KG} kg) to {payload.Orbit} orbit\r\n");
+            }
+
+            if (flightToDisplay.Details != null)
+            {
+                responseBuilder.Append($"\r\n");
+                responseBuilder.Append($"{flightToDisplay.Details}\r\n");
+            }
+
+            responseBuilder.Append($"\r\n");
+            responseBuilder.Append(flightToDisplay.Links.Video_Link != null
+                ? $"{flightToDisplay.Links.Video_Link}\r\n"
+                : $"Video not available yet.\r\n");
+
             await ctx.RespondAsync(responseBuilder.ToString());
         }
     }
