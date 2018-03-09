bc890f6a93635d7a5816dfea16954ad46e6994e4
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-09 23:51:46 +0100

    Add hi command

---
+++ SKNIBot/Core/Commands/HiCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SKNIBot.Core.Commands
+{
+    [CommandsGroup]
+    public class HiCommand
+    {
+        [Command("hi")]
+        [Description("Przywitaj się.")]
+        [Aliases("hello", "cześć")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync("Hi " + ctx.User.Mention);
+        }
+    }
+}
