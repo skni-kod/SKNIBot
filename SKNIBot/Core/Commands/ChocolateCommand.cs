faff82608b7b61e7aaf19808bd0a358b7b457806
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-10 00:19:13 +0100

    Chocolate

---
+++ SKNIBot/Core/Commands/ChocolateCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SKNIBot.Core.Commands
+{
+    [CommandsGroup]
+    public class ChocolateCommand
+    {
+        [Command("chocolate")]
+        [Description("Czekolada!")]
+        public async Task Chocolate(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync("https://www.youtube.com/watch?v=WIKqgE4BwAY");
+        }
+    }
+}
