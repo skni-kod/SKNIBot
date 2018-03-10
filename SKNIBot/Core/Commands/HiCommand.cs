5fdc7b5b7b549d0995ac947fcaf084e39b216403
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-10 23:36:45 +0100

    CHange in h

--- SKNIBot/Core/Commands/HiCommand.cs
+++ SKNIBot/Core/Commands/HiCommand.cs
@@ -1,19 +1,31 @@
 ﻿using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using System.Collections.Generic;
+using System;
 
 namespace SKNIBot.Core.Commands
 {
     [CommandsGroup("Różne")]
     public class HiCommand
     {
+        private List<string> _developers = new List<string> { { "Azux Dario2860" }, { "TemporaryNick7229" }, { "Coedo2037" } };
+
         [Command("cześć")]
         [Description("Przywitaj się.")]
         [Aliases("hello", "hi")]
         public async Task Czesc(CommandContext ctx)
         {
+
             await ctx.TriggerTypingAsync();
-            await ctx.RespondAsync("Cześć " + ctx.User.Mention);
+            if (_developers.Contains(ctx.User.Username + ctx.User.Discriminator ))
+            {
+                await ctx.RespondAsync("Cześć mój programisto " + ctx.User.Mention);
+            }
+            else
+            {
+                await ctx.RespondAsync("Cześć " + ctx.User.Mention);
+            }            
         }
     }
 }
