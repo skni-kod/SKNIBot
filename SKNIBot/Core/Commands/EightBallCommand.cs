d53b0b0fd0324c9468f93491a391a7fb76c895dc
Pawel <dziedzicpm@gmail.com>
2018-03-10 20:31:58 +0100

    started 8Ball and MonthyPython

---
+++ SKNIBot/Core/Commands/EightBallCommand.cs
@@ -0,0 +1,63 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Commands {
+    [CommandsGroup("8Ball")]
+    class EightBallCommand {
+        List<string> _responses;
+        Random _rand;
+
+        public EightBallCommand() {
+            _rand = new Random();
+
+            _responses = new List<string> { "Zapytaj Andrzeja",
+                                            "Masz ty w ogóle rozum i godność człowieka?!",
+                                            "Na pewno być może",
+                                            "Odpowiedź jest bliżej niż myślisz",
+                                            "Zapytaj później"};
+        }
+
+        [Command("8Ball")]
+        [Description("Magic 8 ball odpowie na wszystkie twoje pytania!")]
+        public async Task EightBall(CommandContext ctx, [Description("Twoje pytanie")] string question) {
+            await ctx.TriggerTypingAsync();
+
+            int responseIndex = _rand.Next(0, _responses.Count);
+            string response = string.Format("8Ball mówi: `{0}`", _responses[responseIndex]);
+
+            await ctx.RespondAsync(response);
+        }
+
+        //[Command("8BallAdd")]
+        //[RequirePermissions(DSharpPlus.Permissions.Administrator)]
+        //public async Task EightBallAdd(CommandContext ctx, params string[] newResponse) {
+        //    await ctx.TriggerTypingAsync();
+
+        //    StringBuilder builder = new StringBuilder();
+        //    foreach (var item in newResponse) {
+        //        builder.AppendFormat("{0} ", item);
+        //    }
+
+        //    _responses.Add(builder.ToString());
+
+        //    await ctx.RespondAsync("Odpowiedź dodana");
+        //}
+
+        [Command("8BallList")]
+        public async Task EightBallList(CommandContext ctx) {
+            await ctx.TriggerTypingAsync();
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < _responses.Count; i++) {
+                builder.AppendFormat("{0}: {1}\n", i + 1, _responses[i]);
+            }
+
+            await ctx.RespondAsync(builder.ToString());
+        }
+    }
+}
---
+++ SKNIBot/Core/Commands/MonthyPythonQuatesCommand.cs
@@ -0,0 +1,35 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Commands {
+    //PÓŹNIEJ TO POPRAWIE, NIE BIJCIE :<
+    [CommandsGroup("Cytaty")]
+    class MonthyPythonQuatesCommand {
+        string _grenade;
+        Random _rand;
+
+        public MonthyPythonQuatesCommand() {
+            _rand = new Random();
+
+            _grenade = @"*I Pan powiedział: Wpierw wyjąć musisz świętą Zawleczkę, potem masz zliczyć
+do trzech, nie mniej, nie więcej. Trzy ma być liczbą, do której liczyć masz i
+liczbą tą ma być trzy. Do czterech nie wolno ci liczyć, ani do dwóch.Masz
+tylko policzyć do trzech. Pięć jest wykluczone. Gdy liczba trzy jako trzecia w
+kolejności osiągnięta zostanie, wówczas rzucić masz Święty Granat Ręczny z
+Antiochii w kierunku wroga, co naigrawał się z ciebie w polu widzenia twego, a
+wówczas on kitę odwali. Amen.*";
+        }
+
+        [Command("HolyHandGrenade")]
+        public async Task EightBall(CommandContext ctx) {
+            await ctx.TriggerTypingAsync();
+
+            await ctx.RespondAsync(_grenade);
+        }
+    }
+}
