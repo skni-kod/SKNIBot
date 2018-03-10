bcbaa4732c3e83ac694f065629c90030f117cb8b
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-10 01:30:38 +0100

    Add Shuka really command

---
+++ SKNIBot/Core/Commands/YouTubeCommands/ShukaReally.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace SKNIBot.Core.Commands.YouTubeCommands
+{
+    [CommandsGroup]
+    public class ShukaReallyCommand : PostMovieCommand
+    {
+        [Command("shukareally")]
+        [Description("Shuka really!")]
+        public async Task ShukaReally(CommandContext ctx, [Description("Użytkownik do wzmienienia.")] DiscordMember member = null)
+        {
+            await postMovie(ctx, "https://www.youtube.com/watch?v=DCZPWAnXmx4", member);
+        }
+    }
+}
