7d92fa48f4d35be4e15c0f8976c9f4fff1cd01f3
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-10 01:37:00 +0100

    Add todokete comamnd

---
+++ SKNIBot/Core/Commands/YouTubeCommands/TodoketeCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace SKNIBot.Core.Commands.YouTubeCommands
+{
+    [CommandsGroup]
+    public class TooketeCommand : PostMovieCommand
+    {
+        [Command("todokete")]
+        [Description("Snow halation!")]
+        public async Task Todokete(CommandContext ctx, [Description("Użytkownik do wzmienienia.")] DiscordMember member = null)
+        {
+            await postMovie(ctx, "https://www.youtube.com/watch?v=TnLMlQJHAUU", member);
+        }
+    }
+}
