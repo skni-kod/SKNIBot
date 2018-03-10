9ef17d4a6a3c4e089564f1ba03b186fe5526264c
tearth <Tearth@users.noreply.github.com>
2018-03-10 13:37:53 +0100

    Add YouTube command.

---
+++ SKNIBot/Core/Commands/YouTubeCommands/YouTubeCommand.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Newtonsoft.Json;
+using SKNIBot.Core.Containers.YouTube;
+
+namespace SKNIBot.Core.Commands.YouTubeCommands
+{
+    [CommandsGroup]
+    public class YouTubeCommand
+    {
+        private List<VideoData> _videos;
+        private const string _videosFile = "youtube.json";
+
+        public YouTubeCommand()
+        {
+            using (var file = new StreamReader(_videosFile))
+            {
+                _videos = JsonConvert.DeserializeObject<List<VideoData>>(file.ReadToEnd());
+            }
+        }
+
+        [Command("youtube")]
+        [Description("Display video!")]
+        [Aliases("yt")]
+        public async Task YouTube(CommandContext ctx, string videoName = null, DiscordMember member = null)
+        {
+            await ctx.TriggerTypingAsync();
+            if (videoName == "list")
+            {
+                await ctx.RespondAsync($"Dostępne filmy:\r\n{GetAvailableParameters()}");
+                return;
+            }
+
+            var videoData = _videos.FirstOrDefault(p => p.Name == videoName);
+            if (videoData == null)
+            {
+                await ctx.RespondAsync("Nieznany parametr, wpisz !yt list aby uzyskać listę dostępnych.");
+                return;
+            }
+
+            var response = videoData.Link;
+            if (member != null)
+            {
+                response += $" {member.Mention}";
+            }
+
+            await ctx.RespondAsync(response);
+        }
+
+        private string GetAvailableParameters()
+        {
+            return string.Join(", ", _videos.Select(p => p.Name).ToArray());
+        }
+    }
+}
---
+++ SKNIBot/Core/Containers/YouTube/VideoData.cs
@@ -0,0 +1,8 @@
+﻿namespace SKNIBot.Core.Containers.YouTube
+{
+    public class VideoData
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+}
