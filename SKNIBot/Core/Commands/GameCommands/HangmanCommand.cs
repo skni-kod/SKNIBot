88e74e6b0f1401493c9d081d8c000bc788534c8f
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-11 22:32:15 +0100

    Hangman

--- SKNIBot/Core/Commands/GameCommands/HangmanCommand.cs
+++ SKNIBot/Core/Commands/GameCommands/HangmanCommand.cs
@@ -10,17 +10,42 @@ namespace SKNIBot.Core.Commands.GameCommands
     [CommandsGroup("Gry")]
     public class HangmanCommand
     {
+        private const int Stages = 9;
+
+        private bool _gameStarted;
+        private int _actualStage;
+        private string _word;
+
+        public HangmanCommand()
+        {
+            _gameStarted = false;
+            _actualStage = 0;
+            _word = "";
+        }
         [Command("wisielec")]
         [Description("Gra w wisielca.")]
         [Aliases("hangman")]
-        public async Task Czesc(CommandContext ctx)
+        public async Task Czesc(CommandContext ctx, [Description("Kategoria")] string type = null)
         {
             await ctx.TriggerTypingAsync();
-            string lol = "";
-            for (int i = 0; i < HangmanConst.stages.Length; i++)
-                for (int j = 0; j < HangmanConst.stages[i].Length; j++)
-                    lol += HangmanConst.stages[i][j] += "\n";
-            await ctx.RespondAsync(lol);
+            string output = "";
+            if(_gameStarted == false)
+            {
+                _gameStarted = true;
+                _actualStage = 1;
+            }
+            //losowanie i wyświetlenie słowa
+            _actualStage++; //Inkrementować gdy nie ma słowa
+            for (int j = 0; j < HangmanConst.stages[_actualStage - 1].Length; j++)
+            {
+                output += HangmanConst.stages[_actualStage - 1][j] += "\n";
+            }
+            //W razie przegranej
+            if(_actualStage == Stages)
+            {
+                _gameStarted = false;
+            }
+            await ctx.RespondAsync(output);
         }
     }
 }
