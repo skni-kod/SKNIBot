3f6b355fd79b53f130bb1c04eed65ed07e55e1c4
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-11 18:41:52 +0100

    Jokes

---
+++ SKNIBot/Core/Commands/TextCommands/JokeCommand.cs
@@ -0,0 +1,51 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Newtonsoft.Json;
+using SKNIBot.Core.Containers.TextContainers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Commands.TextCommands
+{
+    [CommandsGroup("Tekst")]
+    public class JokeCommand
+    {
+        private Random _random;
+        private List<JokesData> _jokes;
+        private const string _jokesFile = "jokes.json";
+
+        public JokeCommand()
+        {
+            _random = new Random();
+            using (var file = new StreamReader(_jokesFile))
+            {
+                _jokes = JsonConvert.DeserializeObject<List<JokesData>>(file.ReadToEnd());
+            }
+        }
+
+        [Command("żart")]
+        [Description("Żarty i suchary w postaci tekstu i obrazków.")]
+        [Aliases("suchar", "joke", "itsJoke")]
+        public async Task Joke(CommandContext ctx, [Description("Użytkownik do wzmienienia.")] DiscordMember member = null)
+        {
+            await ctx.TriggerTypingAsync();
+
+            var jokeIndex = _random.Next(0, _jokes.Count);
+            var jokeToDisplay = _jokes[jokeIndex]; 
+            //Jeżeli długość jest jeden nie podano kodu
+            if (member == null)
+            {
+                await ctx.RespondAsync(jokeToDisplay.Joke);
+            }
+            else
+            {
+                await ctx.RespondAsync(jokeToDisplay.Joke + " " + member.Mention);
+            }
+        }
+    }
+}
---
+++ SKNIBot/Core/Containers/TextContainers/JokesData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SKNIBot.Core.Containers.TextContainers
+{
+    public class JokesData
+    {
+        public string Joke { get; set; }
+    }
+}
