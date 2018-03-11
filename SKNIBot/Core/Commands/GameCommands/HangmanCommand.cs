220d0ea781d4b6d5ff80318e374eae660de4a431
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-11 22:11:47 +0100

    Add hangman
    Doesn't work now

---
+++ SKNIBot/Core/Commands/GameCommands/HangmanCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Collections.Generic;
+using System;
+using SKNIBot.Core.Const.GamesConst;
+
+namespace SKNIBot.Core.Commands.GameCommands
+{
+    [CommandsGroup("Gry")]
+    public class HangmanCommand
+    {
+        [Command("wisielec")]
+        [Description("Gra w wisielca.")]
+        [Aliases("hangman")]
+        public async Task Czesc(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            string lol = "";
+            for (int i = 0; i < HangmanConst.stages.Length; i++)
+                for (int j = 0; j < HangmanConst.stages[i].Length; j++)
+                    lol += HangmanConst.stages[i][j] += "\n";
+            await ctx.RespondAsync(lol);
+        }
+    }
+}
