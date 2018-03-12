e70ae36ae72507962f5fb463aebecce9322267be
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-12 23:50:02 +0100

    Add new command
    Counting time to embedded system lectures.

---
+++ SKNIBot/Core/Commands/UniCommands/WhenArmageddonCommand.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Collections.Generic;
+using System;
+using DSharpPlus.Entities;
+
+namespace SKNIBot.Core.Commands.TextCommands
+{
+    [CommandsGroup("Uczelnia")]
+    public class WhenArmageddonCommand
+    {
+        /// <summary>
+        /// Zwraca kiedy są najbliższe wykłady z systemów wbudowanych
+        /// </summary>
+        /// <param name="ctx">Kontekst</param>
+        /// <param name="member">Użytkownik do wzmienienia</param>
+        /// <returns></returns>
+        [Command("kiedyArmageddon")]
+        [Description("Przywitaj się.")]
+        [Aliases("whenArmageddon", "kiedyŚwider", "kiedyWbudowane")]
+        public async Task WhenArmageddon(CommandContext ctx, [Description("Użytkownik do wzmienienia.")] DiscordMember member = null)
+        {
+
+            await ctx.TriggerTypingAsync();
+
+            DateTime today = DateTime.Today;
+            // The (... + 7) % 7 ensures we end up with a value in the range [0, 6]
+            int daysUntilWednesday = ((int)DayOfWeek.Wednesday - (int)today.DayOfWeek + 7) % 7;
+            DateTime nextWednesday = today.AddDays(daysUntilWednesday);
+            DateTime nextLectures = nextWednesday.AddHours(14);
+
+            TimeSpan timeSpan = nextLectures - DateTime.Now;
+
+            if (member == null)
+            {
+                await ctx.RespondAsync("Najbliższe wykłady z systemów wbudowanych są za: " + timeSpan.Days + " dni " +
+                    timeSpan.Hours + " godzin " + timeSpan.Minutes + " minut " + timeSpan.Seconds + " sekund");
+            }
+            else
+            {
+                await ctx.RespondAsync("Najbliższe wykłady z systemów wbudowanych są za: " + timeSpan.Days + " dni " +
+                    timeSpan.Hours + " godzin " + timeSpan.Minutes + " minut " + timeSpan.Seconds + " sekund " + ctx.User.Mention);
+            }
+        }
+    }
+}
