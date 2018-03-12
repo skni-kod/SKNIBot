7d3ba479215c51bb79afc6d4ecab39a15aa4c0ef
AzuxDario <pawel_mibor95@tlen.pl>
2018-03-12 18:25:55 +0100

    Add catalt

---
+++ SKNIBot/Core/Commands/PicturesCommands/NekoAltCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Newtonsoft.Json;
+using SKNIBot.Core.Containers.PicturesContainers;
+
+namespace SKNIBot.Core.Commands.PicturesCommands
+{
+    [CommandsGroup("Obrazki")]
+    public class NekoAltCommand
+    {
+        private const string _randomSiteURL = "http://thecatapi.com/api/images/get?format=src";
+
+        [Command("kotalt")]
+        [Description("Wyświetla słodkie kotki w postaci gifów.")]
+        [Aliases("nekoalt", "catalt")]
+        public async Task NekoAlt(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+
+            var request = (HttpWebRequest)WebRequest.Create(_randomSiteURL);
+            request.AllowAutoRedirect = false;
+
+            using (var response = request.GetResponse())
+            {
+                await ctx.RespondAsync(response.Headers["Location"]);
+            }
+        }
+    }
+}
