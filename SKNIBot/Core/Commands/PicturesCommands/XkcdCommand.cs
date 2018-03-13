39908c03e970e93c71923673d0f413553535147b
tearth <Tearth@users.noreply.github.com>
2018-03-13 17:48:09 +0100

    Add xkcd command.

---
+++ SKNIBot/Core/Commands/PicturesCommands/XkcdCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Newtonsoft.Json;
+using SKNIBot.Core.Containers.PicturesContainers;
+
+namespace SKNIBot.Core.Commands.PicturesCommands
+{
+    [CommandsGroup("Obrazki")]
+    public class XkcdCommand
+    {
+        private Random _random;
+
+        private const string RandomXkcdUrl = "https://xkcd.com/{0}/info.0.json";
+        private const int MaxIndex = 1500;
+
+        public XkcdCommand()
+        {
+            _random = new Random();
+        }
+
+        [Command("xkcd")]
+        [Description("Losowy obrazek z xkcd.")]
+        public async Task Xkcd(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+
+            var randomIndex = _random.Next(0, MaxIndex);
+            var link = string.Format(RandomXkcdUrl, randomIndex);
+
+            var client = new WebClient();
+            var url = client.DownloadString(link);
+            var xkcdContainer = JsonConvert.DeserializeObject<XkcdContainer>(url);
+
+            await ctx.RespondAsync(GetResponse(xkcdContainer));
+        }
+
+        private string GetResponse(XkcdContainer xkcdContainer)
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append($"**{xkcdContainer.Title}**");
+            stringBuilder.Append("\r\n\r\n");
+            stringBuilder.Append(xkcdContainer.Img);
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ SKNIBot/Core/Containers/PicturesContainers/XkcdContainer.cs
@@ -0,0 +1,8 @@
+﻿namespace SKNIBot.Core.Containers.PicturesContainers
+{
+    public class XkcdContainer
+    {
+        public string Title { get; set; }
+        public string Img { get; set; }
+    }
+}
