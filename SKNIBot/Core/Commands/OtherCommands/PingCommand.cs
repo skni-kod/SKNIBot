bf4c67135ec57b0af72deaa0144b60797b71f5f9
tearth <Tearth@users.noreply.github.com>
2018-03-14 18:20:35 +0100

    Add Ping command.

---
+++ SKNIBot/Core/Commands/OtherCommands/PingCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SKNIBot.Core.Commands.OtherCommands
+{
+    [CommandsGroup("Różne")]
+    public class PingCommand
+    {
+        [Command("ping")]
+        [Description("Wyświetl najnowszy ping.")]
+        public async Task Avatar(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync($"{ctx.Client.Ping} ms");
+        }
+    }
+}
