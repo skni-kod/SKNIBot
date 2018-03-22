a88d509fad07b9bb967687a6eb311df823eb6efd
tearth <Tearth@users.noreply.github.com>
2018-03-22 23:49:47 +0100

    Add extension method to randomize row.

---
+++ SKNIBot/Core/Database/Helpers/RandomRowHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Database.Helpers
+{
+    public static class RandomRowHelper
+    {
+        public static T Random<T>(this IDbSet<T> dbSet) where T:class
+        {
+            return dbSet.OrderBy(p => Guid.NewGuid()).FirstOrDefault();
+        }
+    }
+}
