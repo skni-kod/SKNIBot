55af9c8e1d9b7a4bf921f140454e18f6ad019981
tearth <Tearth@users.noreply.github.com>
2018-04-01 17:38:20 +0200

    Add say and sayd commands.

---
+++ SKNIBot/Core/Commands/TextCommands/SayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SKNIBot.Core.Commands.TextCommands
+{
+    [CommandsGroup("Tekst")]
+    public class SayCommand
+    {
+        [Command("mów")]
+        [Description("Każ mi coś powiedzieć!")]
+        [Aliases("mow", "powiedz", "say")]
+        public async Task Say(CommandContext ctx, [Description("Co chcesz powiedzieć?")] string message)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync(message);
+        }
+
+        [Command("mówd")]
+        [Description("Każ mi coś powiedzieć!")]
+        [Aliases("mowd", "powiedzd", "sayd")]
+        public async Task SayD(CommandContext ctx, [Description("Co chcesz powiedzieć?")] string message)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.Channel.DeleteMessageAsync(ctx.Message);
+            await ctx.RespondAsync(message);
+        }
+    }
+}
