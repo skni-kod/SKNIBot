5f5c540642076a8199f0734835862d83cb8ab4b4
AzuxDario <pawel_mibor95@tlen.pl>
2018-04-01 22:30:04 +0200

    Added removeMessage

---
+++ SKNIBot/Core/Commands/ModerationCommands/RemoveMessageCommand.cs
@@ -0,0 +1,26 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Commands.ModerationCommands
+{
+    [CommandsGroup("Moderacja")]
+    class RemoveMessageCommand
+    {
+        [Command("usuńID")]
+        [Aliases("usunID")]
+        [Description("Usuwa konkretną wiadomość.")]
+        [RequireRolesAttribute("Projekt - Bot")]
+        public async Task RemoveTest(CommandContext ctx, [Description("ID wiadomości.")] ulong ID)
+        {
+            var message = await ctx.Channel.GetMessageAsync(ID);
+            await ctx.Channel.DeleteMessageAsync(message);
+            message = await ctx.Channel.GetMessageAsync(ctx.Message.Id);
+            await ctx.Channel.DeleteMessageAsync(message);
+        }
+    }
+}
