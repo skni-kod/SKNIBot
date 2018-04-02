679fdc017ee88004ad8302a2b0873e0581505cfb
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2018-04-02 19:07:54 +0200

    Repaired fun command

--- SKNIBot/Core/Commands/PicturesCommands/NekosLife/LewdCommand.cs
+++ SKNIBot/Core/Commands/PicturesCommands/NekosLife/LewdCommand.cs
@@ -2,6 +2,7 @@
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using SKNIBot.Core.Const.PicturesConst.NekosLife;
 
 namespace SKNIBot.Core.Commands.PicturesCommands.NekosLife
 {
@@ -10,12 +11,14 @@ namespace SKNIBot.Core.Commands.PicturesCommands.NekosLife
     {
         //Z uwagi na możliwy ostry (zboczony) content na razie zakomentowane XD XD
         [Command("lewd")]
-        [Description("Wyświetla obrazki lewd.")]
+        [Description("Wyświetla obrazki lewd.")]        
         public async Task Lewd(CommandContext ctx, [Description("Wzmianka")] DiscordMember member = null)
         {
             await ctx.TriggerTypingAsync();
-            //await SendImage(ctx, NekosLifePicturesEndpoints.Lewd, member);
-            await ctx.RespondAsync("Chciałbyś ;)");
+            if (ctx.Channel.IsNSFW)
+                await SendImage(ctx, NekosLifePicturesEndpoints.Lewd, member);
+            else
+                await ctx.RespondAsync("Chciałbyś ;)");
         }
     }
 }
