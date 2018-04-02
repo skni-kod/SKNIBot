c2891de9e45b3d4cdbad6621479ef322056f4f57
AzuxDario <pawel_mibor95@tlen.pl>
2018-04-02 15:47:28 +0200

    Added removeToMessage

---
+++ SKNIBot/Core/Commands/ModerationCommands/RemoveMessagesToCommand.cs
@@ -0,0 +1,31 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Commands.ModerationCommands
+{
+    [CommandsGroup("Moderacja")]
+    class RemoveMessagesToCommand
+    {
+        [Command("usuńDoID")]
+        [Aliases("usunDoID")]
+        [Description("Usuwa wiadomości do wiadomości o podanym id włącznie.")]
+        [RequireRolesAttribute("Projekt - Bot")]
+        public async Task RemoveLastMessagesTo(CommandContext ctx, [Description("Liczba ostatnich wiadomości do usunięcia.")] ulong messageID)
+        {
+            var messages = await ctx.Channel.GetMessagesAsync(1);
+            do
+            {
+                await ctx.Channel.DeleteMessageAsync(messages.First());
+                messages = await ctx.Channel.GetMessagesAsync(1);
+                if (messages.Count == 0) break;
+            }
+            while (messages.First().Id != messageID);
+            
+        }
+    }
+}
