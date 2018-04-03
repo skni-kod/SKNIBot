0ff0557e3854695eab5c0b3e73308dcca248eb24
AzuxDario <pawel_mibor95@tlen.pl>
2018-04-03 23:49:11 +0200

    Changes in HiCommand
    Add one more developer and change nick & discriminator to ID.

--- SKNIBot/Core/Commands/TextCommands/HiCommand.cs
+++ SKNIBot/Core/Commands/TextCommands/HiCommand.cs
@@ -8,7 +8,12 @@ namespace SKNIBot.Core.Commands.TextCommands
     [CommandsGroup("Tekst")]
     public class HiCommand
     {
-        private List<string> _developers = new List<string> { "Azux Dario2860", "TemporaryNick7229", "Coedo2037"};
+        private List<ulong> _developers = new List<ulong> {
+            386559253088829440,//Azux Dario#2860
+            352775074513682434,//TemporaryNick#7229
+            305642795706744833,//Coedo#2037
+            231846704947658752//Szatku#4105
+        };
 
         [Command("cześć")]
         [Description("Przywitaj się.")]
@@ -16,8 +21,7 @@ namespace SKNIBot.Core.Commands.TextCommands
         public async Task Czesc(CommandContext ctx)
         {
             await ctx.TriggerTypingAsync();
-
-            if (_developers.Contains(ctx.User.Username + ctx.User.Discriminator ))
+            if (_developers.Contains(ctx.User.Id))
             {
                 await ctx.RespondAsync("Cześć mój programisto " + ctx.User.Mention);
             }
