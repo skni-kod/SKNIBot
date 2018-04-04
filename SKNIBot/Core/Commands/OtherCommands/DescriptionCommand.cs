60c0ca024d749c63e8b64baea84b7c9d90e1e68d
tearth <Tearth@users.noreply.github.com>
2018-04-04 13:36:47 +0200

    Add description refresh (due to API restrictions).

--- SKNIBot/Core/Commands/OtherCommands/DescriptionCommand.cs
+++ SKNIBot/Core/Commands/OtherCommands/DescriptionCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
@@ -8,12 +9,41 @@ namespace SKNIBot.Core.Commands.OtherCommands
     [CommandsGroup("Różne")]
     public class DescriptionCommand
     {
+        private Timer _refreshDescriptionTimer;
+        private int _refreshDescriptionInterval;
+
+        private CommandContext _ctx;
+        private string _description;
+
+        public DescriptionCommand()
+        {
+            _refreshDescriptionInterval = 1000 * 60;    // every 1 minute
+
+            _ctx = null;
+            _description = string.Empty;
+
+            _refreshDescriptionTimer = new Timer(RefreshDescriptionCallback, null, _refreshDescriptionInterval, Timeout.Infinite);
+        }
+
         [Command("opis")]
         [Description("Zmienia opis bota.")]
         [RequireRolesAttribute("Projekt - Bot")]
         public async Task Description(CommandContext ctx, [Description("Nowy opis.")] string description = null)
         {
+            _ctx = ctx;
+            _description = description;
+
             await ctx.Client.UpdateStatusAsync(new DiscordGame(description));
+        }
+
+        private void RefreshDescriptionCallback(object state)
+        {
+            if (_ctx != null && _description != string.Empty)
+            {
+                _ctx.Client.UpdateStatusAsync(new DiscordGame(_description));
+            }
+
+            _refreshDescriptionTimer.Change(_refreshDescriptionInterval, Timeout.Infinite);
         }
     }
 }
