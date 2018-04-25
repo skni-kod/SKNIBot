0c6ba90c6e1118131f93724b1e7bd00af50ec80c
tearth <Tearth@users.noreply.github.com>
2018-04-25 20:57:20 +0200

    Add CompileRequestContainer which will be send as request to API.

---
+++ SKNIBot/Core/Commands/OtherCommands/CompileCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SKNIBot.Core.Commands.OtherCommands
+{
+    [CommandsGroup("Other")]
+    public class CompileCommand
+    {
+        [Command("compile")]
+        [Description("asd")]
+        public async Task SpaceX(CommandContext ctx, string code = null, string input)
+        {
+            await ctx.TriggerTypingAsync();
+        }
+    }
+}
---
+++ SKNIBot/Core/Containers/CompilationContainers/CompileRequestContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Containers.CompilationContainers
+{
+    public class CompileRequestContainer
+    {
+        public string ClientID { get; set; }
+        public string ClientSecret { get; set; }
+        public string Script { get; set; }
+        public string StdIn { get; set; }
+        public string Language { get; set; }
+        public string VersionIndex { get; set; }
+    }
+}
