cccc61ba27420e7db75d3a8892b71b14b444a5f9
tearth <Tearth@users.noreply.github.com>
2018-04-25 22:53:26 +0200

    Update Compile command to workable version.

--- SKNIBot/Core/Commands/OtherCommands/CompileCommand.cs
+++ SKNIBot/Core/Commands/OtherCommands/CompileCommand.cs
@@ -1,21 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Newtonsoft.Json;
+using SKNIBot.Core.Containers.CompilationContainers;
+using SKNIBot.Core.Settings;
 
 namespace SKNIBot.Core.Commands.OtherCommands
 {
     [CommandsGroup("Other")]
     public class CompileCommand
     {
+        private const string ApiEndpoint = "https://api.jdoodle.com/v1/execute";
+
         [Command("compile")]
         [Description("asd")]
-        public async Task SpaceX(CommandContext ctx, string code = null, string input)
+        public async Task Compile(CommandContext ctx)
         {
             await ctx.TriggerTypingAsync();
+
+            var splittedInput = ctx.RawArgumentString.Split(',').ToList();
+            var language = splittedInput[0].Trim();
+            var input = splittedInput[1].Trim();
+            var code = splittedInput[2].Trim();
+
+            var compilationResult = GetCompilationResult(language, input, code);
+            var compilationEmbedResult = GetEmbedResult(compilationResult);
+
+            await ctx.RespondAsync("", false, compilationEmbedResult);
+        }
+
+        private CompilationResultContainer GetCompilationResult(string language, string input, string code)
+        {
+            var compileRequest = new CompileRequestContainer
+            {
+                clientId = SettingsLoader.Container.JDoodle_Client_ID,
+                clientSecret = SettingsLoader.Container.JDoodle_Client_Secret,
+                language = language,
+                script = code,
+                stdin = input,
+                versionIndex = "0"
+            };
+
+            var compileRequestJSON = JsonConvert.SerializeObject(compileRequest);
+
+            var webClient = new WebClient();
+            webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+
+            var response = webClient.UploadString(ApiEndpoint, compileRequestJSON);
+
+            return JsonConvert.DeserializeObject<CompilationResultContainer>(response);
+        }
+
+        private DiscordEmbed GetEmbedResult(CompilationResultContainer compilationResult)
+        {
+            var embedResult = new DiscordEmbedBuilder();
+            embedResult.AddField("Rezultat", compilationResult.output);
+
+            return embedResult;
         }
     }
 }
