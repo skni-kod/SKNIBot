78140cf9135cc180138c5b21f429f8bd7b289faf
tearth <Tearth@users.noreply.github.com>
2018-04-25 21:37:23 +0200

    Add compilation result container.

---
+++ SKNIBot/Core/Containers/CompilationContainers/CompilationResultContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Containers.CompilationContainers
+{
+    public class CompilationResultContainer
+    {
+        public string output { get; set; }
+        public int statusCode { get; set; }
+        public string memory { get; set; }
+        public string cpuTime { get; set; }
+    }
+}
