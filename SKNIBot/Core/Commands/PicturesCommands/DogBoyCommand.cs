14f1d55cbfc92da0a3d8f7d0e54cb7f4d8a9c8bc
AzuxDario <pawel_mibor95@tlen.pl>
2018-05-17 13:19:09 +0200

    Add picture commands for future pruposes

---
+++ SKNIBot/Core/Commands/PicturesCommands/DogBoyCommand.cs
@@ -0,0 +1,49 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using SKNIBot.Core.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Commands.PicturesCommands
+{
+    //[CommandsGroup("Obrazki")]
+    class DogBoyCommand
+    {
+        private Random random;
+
+        public DogBoyCommand()
+        {
+            random = new Random();
+        }
+
+        //[Command("catboy")]
+        //[Description("Wyświetla słodkie dogboy.")]
+        public async Task Dog(CommandContext ctx, [Description("Wzmianka")] DiscordMember member = null)
+        {
+            await ctx.TriggerTypingAsync();
+
+            using (var databaseContext = new StaticDBContext())
+            {
+                // String.Equals doesn't work in SQLite provider (comparison is case sensitive) so it must be replaced with DbFunctions.Like().
+                var dogBoys = databaseContext.SimpleResponses
+                    .Where(catboy => catboy.Command.Name == "DogBoy")
+                    .Select(p => p.Content)
+                    .ToList();
+
+                var wordIndex = random.Next(0, dogBoys.Count);
+                var response = dogBoys[wordIndex];
+
+                if (member != null)
+                {
+                    response += $" {member.Mention}";
+                }
+
+                await ctx.RespondAsync(response);
+            }
+        }
+    }
+}
---
+++ SKNIBot/Core/Commands/PicturesCommands/DogGirlCommand.cs
@@ -0,0 +1,49 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using SKNIBot.Core.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Commands.PicturesCommands
+{
+    //[CommandsGroup("Obrazki")]
+    class DogGirlCommand
+    {
+        private Random random;
+
+        public DogGirlCommand()
+        {
+            random = new Random();
+        }
+
+        //[Command("catboy")]
+        //[Description("Wyświetla słodkie doggirl.")]
+        public async Task Dog(CommandContext ctx, [Description("Wzmianka")] DiscordMember member = null)
+        {
+            await ctx.TriggerTypingAsync();
+
+            using (var databaseContext = new StaticDBContext())
+            {
+                // String.Equals doesn't work in SQLite provider (comparison is case sensitive) so it must be replaced with DbFunctions.Like().
+                var dogGirl = databaseContext.SimpleResponses
+                    .Where(catboy => catboy.Command.Name == "DogGirl")
+                    .Select(p => p.Content)
+                    .ToList();
+
+                var wordIndex = random.Next(0, dogGirl.Count);
+                var response = dogGirl[wordIndex];
+
+                if (member != null)
+                {
+                    response += $" {member.Mention}";
+                }
+
+                await ctx.RespondAsync(response);
+            }
+        }
+    }
+}
