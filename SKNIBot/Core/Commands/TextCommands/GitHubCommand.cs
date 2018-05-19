882250c4a6cd110de518b9bad2203b82a6a26591
tearth <Tearth@users.noreply.github.com>
2018-05-19 21:28:57 +0200

    Add GitHub command to display random repository.

---
+++ SKNIBot/Core/Commands/TextCommands/GitHubCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Newtonsoft.Json;
+using SKNIBot.Core.Containers.TextContainers;
+
+namespace SKNIBot.Core.Commands.TextCommands
+{
+    [CommandsGroup("Tekst")]
+    public class GitHubCommand
+    {
+        private Random _random;
+        private const int MaxRepositoryID = 10000000;
+
+        public GitHubCommand()
+        {
+            _random = new Random();
+        }
+
+        [Command("github")]
+        [Description("Wyświetla link do losowego repozytorium na GitHubie.")]
+        public async Task GitHub(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+
+            var repositoryID = _random.Next(MaxRepositoryID);
+            var requestLink = $"https://api.github.com/repositories?since={repositoryID}";
+
+            var webClient = new WebClient();
+            webClient.Headers["User-Agent"] = "Bot";
+
+            var response = webClient.DownloadString(requestLink);
+
+            var responseContainer = JsonConvert.DeserializeObject<List<GitHubContainer>>(response);
+            var repositoryLink = responseContainer[0].html_url;
+
+            await ctx.RespondAsync(repositoryLink);
+        }
+    }
+}
---
+++ SKNIBot/Core/Containers/TextContainers/GitHubContainer.cs
@@ -0,0 +1,7 @@
+﻿namespace SKNIBot.Core.Containers.TextContainers
+{
+    public class GitHubContainer
+    {
+        public string html_url { get; set; }
+    }
+}
