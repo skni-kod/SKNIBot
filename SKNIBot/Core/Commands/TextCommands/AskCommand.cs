55a16d5e9b59082f31973c75a14cba370ecfe424
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2018-06-26 16:22:43 +0200

    Created command

---
+++ SKNIBot/Core/Commands/TextCommands/AskCommand.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Newtonsoft.Json;
+using SKNIBot.Core.Containers.TextContainers;
+
+namespace SKNIBot.Core.Commands.TextCommands
+{
+    [CommandsGroup("Różne")]
+    class AskCommand
+    {
+        [Command("ask")]
+        [Description("Zapytaj o coś bota.")]
+        public async Task Avatar(CommandContext ctx, [Description("Pytanie")] string question = null)
+        {
+            await ctx.TriggerTypingAsync();
+
+            if(question == null)
+            {
+                await ctx.RespondAsync("Składnia to `!ask {pytanie}`");
+            }
+            else if(!question.Contains("?"))
+            {
+                await ctx.RespondAsync("A może jednak zapytasz zamiast stawiać twierdzenia?");
+            }
+            else
+            {
+                var client = new WebClient();
+
+                var response = client.DownloadString("https://yesno.wtf/api");
+                var answer = JsonConvert.DeserializeObject<AskContainer>(response);
+                var answerPicture = client.DownloadData(answer.image);
+                var stream = new MemoryStream(answerPicture);
+
+                await ctx.RespondWithFileAsync(stream, "answer.gif");
+            }
+        }
+    }
+}
