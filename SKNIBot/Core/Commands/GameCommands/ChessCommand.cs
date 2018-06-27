3a381a03cb60f40e9a8cb273581771107d1bc00f
tearth <Tearth@users.noreply.github.com>
2018-06-27 12:58:55 +0200

    Add board drawing.

--- SKNIBot/Core/Commands/GameCommands/ChessCommand.cs
+++ SKNIBot/Core/Commands/GameCommands/ChessCommand.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 
@@ -11,12 +15,41 @@ namespace SKNIBot.Core.Commands.GameCommands
     [CommandsGroup("Gry")]
     public class ChessCommand
     {
+        public ChessCommand()
+        {
+
+        }
+
         [Command("szachy")]
         [Description("Dev.")]
         [Aliases("chess", "c")]
-        public async Task Hangman(CommandContext ctx)
+        public async Task Chess(CommandContext ctx)
         {
-            await ctx.RespondAsync("test");
+            var f1 = Image.FromFile("Images/Chess/field1.png");
+            var f2 = Image.FromFile("Images/Chess/field2.png");
+
+            var board = new Bitmap(512, 512);
+            var graphic = Graphics.FromImage(board);
+
+            var odd = false;
+            for (var x = 0; x < 8; x++)
+            {
+                for (var y = 0; y < 8; y++)
+                {
+                    var field = odd ? f1 : f2;
+                    graphic.DrawImage(field, new Point(x * 64, y * 64));
+
+                    odd = !odd;
+                }
+
+                odd = !odd;
+            }
+
+            var stream = new MemoryStream();
+            board.Save(stream, ImageFormat.Png);
+            stream.Position = 0;
+
+            await ctx.RespondWithFileAsync(stream, "test.png");
         }
     }
 }
