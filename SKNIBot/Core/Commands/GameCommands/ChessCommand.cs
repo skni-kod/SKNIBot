edf644bea09e4f278673bd406a1f9885a21e2347
tearth <Tearth@users.noreply.github.com>
2018-06-27 12:42:11 +0200

    Add chess textures.

---
+++ SKNIBot/Core/Commands/GameCommands/ChessCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace SKNIBot.Core.Commands.GameCommands
+{
+    [CommandsGroup("Gry")]
+    public class ChessCommand
+    {
+        [Command("szachy")]
+        [Description("Dev.")]
+        [Aliases("chess", "c")]
+        public async Task Hangman(CommandContext ctx)
+        {
+            await ctx.RespondAsync("test");
+        }
+    }
+}
