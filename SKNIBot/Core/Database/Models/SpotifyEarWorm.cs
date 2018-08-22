75542be26eb6d54aee7cb1ffc0e949360a920cae
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2018-08-22 22:18:54 +0200

    Created Command EarWorm

---
+++ SKNIBot/Core/Database/Models/SpotifyEarWorm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SKNIBot.Core.Database.Models
+{
+    public enum TypeOfPeriod {DAY, WEEK, MONTH, YEAR}
+    public class SpotifyEarWorm
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public String Uri { get; set; }
+        public DateTime DayStamp { get; set; }
+        public TypeOfPeriod Category { get; set; }
+    }
+}
