317db92f0444e18f35df61ceaa38955b6aec4921
tearth <Tearth@users.noreply.github.com>
2018-08-26 18:40:32 +0200

    Add statsall command.

--- SKNIBot/Core/Commands/ModerationCommands/StatsCommand.cs
+++ SKNIBot/Core/Commands/ModerationCommands/StatsCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -6,13 +7,14 @@ using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using DSharpPlus.Exceptions;
 
 namespace SKNIBot.Core.Commands.ModerationCommands
 {
     [CommandsGroup("Moderacja")]
     public class MessageStatsCommand : BaseCommandModule
     {
-        private const int UsernameFieldLength = 20;
+        private const int UsernameFieldLength = 40;
         private const int MessagesCountFieldLength = 20;
 
         private int TotalFieldsLength => UsernameFieldLength + MessagesCountFieldLength;
@@ -24,21 +26,90 @@ namespace SKNIBot.Core.Commands.ModerationCommands
         {
             await ctx.TriggerTypingAsync();
 
-            var messages = await GetAllMessagesFromChannel(ctx);
+            await ctx.RespondAsync("To chwilę potrwa... :eyes:");
+
+            var messages = await GetAllMessagesFromChannel(ctx.Channel);
             var userMessagesStats = CountUserMessages(messages);
-            var response = GetResponse(userMessagesStats, ctx.Channel.Name);
+            var response = await GetResponse(userMessagesStats, ctx.Channel.Name, ctx.Guild);
 
             await ctx.RespondAsync(response);
         }
 
-        private async Task<List<DiscordMessage>> GetAllMessagesFromChannel(CommandContext ctx)
+        [Command("statsall")]
+        [Description("Wyświetla statystyki dla wszystkich kanałów.")]
+        [RequirePermissions(Permissions.ManageMessages)]
+        public async Task StatsAll(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync("To chwilę potrwa... :eyes:");
+
+            var allMessages = new List<DiscordMessage>();
+
+            foreach (var channel in ctx.Guild.Channels.Where(p => p.Type == ChannelType.Text))
+            {
+                try
+                {
+                    var messages = await GetAllMessagesFromChannel(channel);
+                    allMessages.AddRange(messages);
+                }
+                catch (UnauthorizedException ex)
+                {
+                  /*        Brak dostępu
+                    __________████████_____██████
+                    _________█░░░░░░░░██_██░░░░░░█
+                    ________█░░░░░░░░░░░█░░░░░░░░░█
+                    _______█░░░░░░░███░░░█░░░░░░░░░█
+                    _______█░░░░███░░░███░█░░░████░█
+                    ______█░░░██░░░░░░░░███░██░░░░██
+                    _____█░░░░░░░░░░░░░░░░░█░░░░░░░░███
+                    ____█░░░░░░░░░░░░░██████░░░░░████░░█
+                    ____█░░░░░░░░░█████░░░████░░██░░██░░█
+                    ___██░░░░░░░███░░░░░░░░░░█░░░░░░░░███
+                    __█░░░░░░░░░░░░░░█████████░░█████████
+                    _█░░░░░░░░░░█████_████___████_█████___█
+                    _█░░░░░░░░░░█______█_███__█_____███_█___█
+                     █░░░░░░░░░░░░█___████_████____██_██████
+                      ░░░░░░░░░░░░░█████████░░░████████░░░█
+                      ░░░░░░░░░░░░░░░░█░░░░░█░░░░░░░░░░░░█
+                      ░░░░░░░░░░░░░░░░░░░░██░░░░█░░░░░░██
+                      ░░░░░░░░░░░░░░░░░░██░░░░░░░███████
+                      ░░░░░░░░░░░░░░░░██░░░░░░░░░░█░░░░░█
+                      ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█
+                      ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█
+                      ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█
+                      ░░░░░░░░░░░█████████░░░░░░░░░░░░░░██
+                      ░░░░░░░░░░█▒▒▒▒▒▒▒▒███████████████▒▒█
+                      ░░░░░░░░░█▒▒███████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+                      ░░░░░░░░░█▒▒▒▒▒▒▒▒▒█████████████████
+                      ░░░░░░░░░░████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+                      ░░░░░░░░░░░░░░░░░░██████████████████
+                      ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█
+                      ██░░░░░░░░░░░░░░░░░░░░░░░░░░░██
+                      ▓██░░░░░░░░░░░░░░░░░░░░░░░░██
+                      ▓▓▓███░░░░░░░░░░░░░░░░░░░░█
+                      ▓▓▓▓▓▓███░░░░░░░░░░░░░░░██
+                      ▓▓▓▓▓▓▓▓▓███████████████▓▓█
+                      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██
+                      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█
+                      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█
+                   */
+                }
+            }
+
+            var userMessagesStats = CountUserMessages(allMessages);
+            var response = await GetResponse(userMessagesStats, "all", ctx.Guild);
+
+            await ctx.RespondAsync(response);
+        }
+
+        private async Task<List<DiscordMessage>> GetAllMessagesFromChannel(DiscordChannel channel)
         {
             var messagesList = new List<DiscordMessage>();
-            var lastMessageID = ctx.Message.Id;
+            var lastMessageID = (await channel.GetMessagesAsync(1))[0].Id;
 
             while (true)
             {
-                var last100Messages = await ctx.Channel.GetMessagesBeforeAsync(lastMessageID, 100);
+                var last100Messages = await channel.GetMessagesBeforeAsync(lastMessageID, 100);
                 messagesList.AddRange(last100Messages);
 
                 if (last100Messages.Count < 100)
@@ -52,24 +123,24 @@ namespace SKNIBot.Core.Commands.ModerationCommands
             return messagesList;
         }
 
-        private List<KeyValuePair<string, int>> CountUserMessages(List<DiscordMessage> messages)
+        private List<KeyValuePair<ulong, int>> CountUserMessages(List<DiscordMessage> messages)
         {
-            var stats = new Dictionary<string, int>();
+            var stats = new Dictionary<ulong, int>();
 
             foreach (var message in messages)
             {
-                if (!stats.ContainsKey(message.Author.Username))
+                if (!stats.ContainsKey(message.Author.Id))
                 {
-                    stats[message.Author.Username] = 0;
+                    stats[message.Author.Id] = 0;
                 }
 
-                stats[message.Author.Username]++;
+                stats[message.Author.Id]++;
             }
 
             return stats.OrderByDescending(p => p.Value).ToList();
         }
 
-        private string GetResponse(List<KeyValuePair<string, int>> userMessagesStats, string channelName)
+        private async Task<string> GetResponse(List<KeyValuePair<ulong, int>> userMessagesStats, string channelName, DiscordGuild guild)
         {
             var response = new StringBuilder();
 
@@ -81,12 +152,20 @@ namespace SKNIBot.Core.Commands.ModerationCommands
             response.Append(new string('-', TotalFieldsLength));
             response.Append("\n");
 
-            foreach (var user in userMessagesStats)
+            foreach (var user in userMessagesStats.Where(p => p.Value >= 10))
             {
-                var userName = user.Key.PadRight(UsernameFieldLength);
-                var userMessagesCount = user.Value.ToString().PadRight(MessagesCountFieldLength);
+                try
+                {
+                    var displayName = (await guild.GetMemberAsync(user.Key)).DisplayName;
+                    var userName = displayName.PadRight(UsernameFieldLength);
+                    var userMessagesCount = user.Value.ToString().PadRight(MessagesCountFieldLength);
 
-                response.Append($"{userName}{userMessagesCount}\n");
+                    response.Append($"{userName}{userMessagesCount}\n");
+                }
+                catch (NotFoundException ex)
+                {
+
+                }
             }
 
             response.Append("```");
