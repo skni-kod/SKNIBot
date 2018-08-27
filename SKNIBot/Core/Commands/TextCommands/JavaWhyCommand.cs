40195fdc61e7f5f3a6918e17fd8a478b40aba354
Paweł Dziedzic <blavikenn@gmail.com>
2018-08-27 14:39:08 +0200

    Reafactored JavaWhy command (added random images) and added JavaWhyAdd command

--- SKNIBot/Core/Commands/TextCommands/JavaWhyCommand.cs
+++ SKNIBot/Core/Commands/TextCommands/JavaWhyCommand.cs
@@ -8,11 +8,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DSharpPlus;
+using SKNIBot.Core.Database.Models;
+using SKNIBot.Core.Extensions;
 
 namespace SKNIBot.Core.Commands.TextCommands
 {
-    //https://cdn.discordapp.com/attachments/420661733544427520/482237629224714260/MEMEM.jpg
-
     [CommandsGroup("Tekst")]
     public class JavaWhyCommand : BaseCommandModule
     {
@@ -24,26 +25,53 @@ namespace SKNIBot.Core.Commands.TextCommands
         }
 
         [Command("JavaWhy")]
+        [Description("Wszystkie znalezione dziwactwa Javy w jednym miejscu!")]
         public async Task JavaWhy(CommandContext ctx)
         {
             await ctx.TriggerTypingAsync();
 
-            using(var db = new DynamicDBContext())
+            var embed = new DiscordEmbedBuilder()
             {
-                var things = db.JavaThings.ToList();
+                Color = ColorHelper.RandomColor()
+            };
 
-                int randIndex = _random.Next(0, things.Count);
+            //Find random java shenaningans in dynamic database
+            using (var db = new DynamicDBContext())
+            {
+                var thingsList = db.JavaThings.ToList();
+                var thing = thingsList.RandomItem();
 
-                var thing = things[randIndex];
-
-                DiscordEmbedBuilder embed = new DiscordEmbedBuilder()
-                {
-                    Color = ColorHelper.RandomColor(),
-                    ImageUrl = "https://cdn.discordapp.com/attachments/420661733544427520/482237629224714260/MEMEM.jpg"
-                };
                 embed.AddField(thing.Title, thing.Content);
+            }
+            
+            //find random image with JavaWhy category
+            using (var db = new StaticDBContext())
+            {
+                var links = db.Media.Where(m => m.Command.Name == "JavaWhy" && m.Category.Name == "JavaWhy")
+                    .Select(m => m.Link).ToList();
 
-                await ctx.RespondAsync(embed: embed);
+                embed.ImageUrl = links.RandomItem();
+            }
+
+            await ctx.RespondAsync(embed: embed);
+        }
+
+        [Command("JavaWhyAdd")]
+        [Description("Dodaj dziwactwo")]
+        [RequireUserPermissions(Permissions.Administrator)]
+        public async Task JavaWhyAdd(CommandContext ctx, string title, string content)
+        {
+            await ctx.TriggerTypingAsync();
+            using (var db = new DynamicDBContext())
+            {
+                db.JavaThings.Add(new JavaThing()
+                {
+                    Title = title,
+                    Content = content
+                });
+                
+                await db.SaveChangesAsync();
+                await ctx.RespondAsync("Dodano kolejne dziwactwo");
             }
         }
     }
