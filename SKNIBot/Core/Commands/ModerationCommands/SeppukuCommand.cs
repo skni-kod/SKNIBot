0d9e565ce3e40c74e54a46715ffa80c1aa7bd1d2
Paweł Dziedzic <blavikenn@gmail.com>
2018-08-27 12:01:38 +0200

    Added Seppuku command (not tested yet - no one wants to use it)

---
+++ SKNIBot/Core/Commands/ModerationCommands/SeppukuCommand.cs
@@ -0,0 +1,23 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Commands.ModerationCommands
+{
+    [CommandsGroup("Moderacja")]
+    public class SeppukuCommand : BaseCommandModule
+    {
+        [Command("Seppuku")]
+        [Description("Zgiń honorową śmiercią!")]
+        public async Task Seppuku(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+
+            await ctx.Member.RemoveAsync("Popełnił Seppuku. Gloria Victis!");
+            await ctx.RespondAsync($"{ctx.Member.DisplayName} popełnił Seppuku");
+        }
+    }
+}
