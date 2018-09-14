dcdf3719baa7e4e7909286b920f113ff3d04fc72
AzuxDario <pawel_mibor95@tlen.pl>
2018-09-14 23:25:53 +0200

    Add post picture helper

---
+++ SKNIBot/Core/Helpers/PostPictureHelper.cs
@@ -0,0 +1,23 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Helpers
+{
+    static class PostPictureHelper
+    {
+        public static async Task PostPicture(CommandContext ctx, string link)
+        {
+            var embed = new DiscordEmbedBuilder
+            {
+                Color = new DiscordColor("#5588EE"),
+                ImageUrl = link
+            };
+
+            await ctx.RespondAsync(null, false, embed);
+        }
+    }
+}
