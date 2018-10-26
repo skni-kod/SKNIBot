cd15f4de9c885f9260a0a83d76b50111d91b1745
AzuxDario <pawel_mibor95@tlen.pl>
2018-10-26 23:16:13 +0200

    Add few responses

--- SKNIBot/Core/MessageResponds/SomeoneCalledMessageRespond.cs
+++ SKNIBot/Core/MessageResponds/SomeoneCalledMessageRespond.cs
@@ -15,6 +15,26 @@ namespace SKNIBot.Core.MessageResponds
                 await args.Channel.TriggerTypingAsync();
                 await args.Channel.SendMessageAsync("Ktoś mnie wołał?");
             }
+            if (args.Message.Content.ToLower().Contains("zensoku zenshin"))
+            {
+                await args.Channel.TriggerTypingAsync();
+                await args.Channel.SendMessageAsync("yousoro!");
+            }
+            if (args.Message.Content.ToLower().Contains("kan kan"))
+            {
+                await args.Channel.TriggerTypingAsync();
+                await args.Channel.SendMessageAsync("mikan");
+            }
+            if (args.Message.Content.ToLower().Contains("yoshiko"))
+            {
+                await args.Channel.TriggerTypingAsync();
+                await args.Channel.SendMessageAsync("Yohane!");
+            }
+            if (args.Message.Content.ToLower().Contains("somebody"))
+            {
+                await args.Channel.TriggerTypingAsync();
+                await args.Channel.SendMessageAsync("https://www.youtube.com/watch?v=L_jWHffIx5E");
+            }
         }
     }
 }
