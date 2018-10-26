144328ae43df322e00df6bf662d9cca2ed385b23
AzuxDario <pawel_mibor95@tlen.pl>
2018-10-26 20:04:03 +0200

    Add new response

---
+++ SKNIBot/Core/MessageResponds/SomeoneCalledMessageRespond.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.MessageResponds
+{
+    public class SomeoneCalledMessageRespond
+    {
+        [MessageRespond]
+        public static async Task Respond(DSharpPlus.EventArgs.MessageCreateEventArgs args)
+        {
+            if (args.Message.Content.ToLower().Contains("bocik"))
+            {
+                await args.Channel.TriggerTypingAsync();
+                await args.Channel.SendMessageAsync("Ktoś mnie wołał?");
+            }
+        }
+    }
+}
