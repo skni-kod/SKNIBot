a64bb12bba3910e042d9c2d885324052f2f6c811
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2018-11-15 23:06:15 +0100

    Added dzwonnik command

---
+++ SKNIBot/Core/Commands/YouTubeCommands/TubularBellsCommand.cs
@@ -0,0 +1,33 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Commands.YouTubeCommands
+{
+    [CommandsGroup("YouTube")]
+    class TubularBellsCommand : BaseCommandModule
+    {
+        static List<string> list = new List<string>(new string[] {
+            "https://youtu.be/enuOArEfqGo?t=40",
+            "https://youtu.be/enuOArEfqGo?t=65",
+            "https://youtu.be/enuOArEfqGo?t=160",
+            "https://youtu.be/enuOArEfqGo?t=185"
+        }
+        );
+        [Command("dzwonnik")]
+        [Description("Taki tam dzwonnik rurowy")]
+        public async Task Avatar(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            Random rnd = new Random();
+            int index = rnd.Next(0, list.Count);
+
+            await ctx.RespondAsync("A trzeba było zostać dzwonnikiem rurowym...\n" + list[index]);
+
+        }
+    }
+}
