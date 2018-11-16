f3799b29e698bf8d4448fd3d9266cac138b6cc89
tearth <Tearth@users.noreply.github.com>
2018-11-16 14:27:05 +0100

    Add method to remove role.

--- SKNIBot/Core/Commands/OtherCommands/RoleCommand.cs
+++ SKNIBot/Core/Commands/OtherCommands/RoleCommand.cs
@@ -47,7 +47,28 @@ namespace SKNIBot.Core.Commands.OtherCommands
         {
             using (var databaseContext = new StaticDBContext())
             {
+                var role = ctx.Guild.Roles.FirstOrDefault(p => p.Name.Equals(name, StringComparison.CurrentCultureIgnoreCase));
 
+                if (role == null)
+                {
+                    await ctx.RespondAsync("Nie ma takiej roli!");
+                    return;
+                }
+
+                if (!databaseContext.Roles.Any(p => p.RoleId == role.Id.ToString()))
+                {
+                    await ctx.RespondAsync("Nie możesz zostać usunięty z tej roli!");
+                    return;
+                }
+
+                if (!ctx.Member.Roles.Contains(role))
+                {
+                    await ctx.RespondAsync("Nie jesteś przypisany do tej roli!");
+                    return;
+                }
+
+                await ctx.Member.RevokeRoleAsync(role, "Usunięta przez Bocika");
+                await ctx.RespondAsync("Rola usunięta!");
             }
         }
 
