88f1af17932fd091bd730d61b38dfc7cfadabd5e
Paweł Dziedzic <blavikenn@gmail.com>
2018-11-28 11:35:01 +0100

    Kiss command unarmed

--- SKNIBot/Core/Commands/PicturesCommands/NekosLife/KissCommand.cs
+++ SKNIBot/Core/Commands/PicturesCommands/NekosLife/KissCommand.cs
@@ -6,18 +6,18 @@ using SKNIBot.Core.Const.PicturesConst.NekosLife;
 
 namespace SKNIBot.Core.Commands.PicturesCommands.NekosLife
 {
-    [CommandsGroup("Obrazki")]
-    public class KissCommand : NekosLifeImage
-    {
-        [Command("kiss")]
-        [Description("Wyświetla obrazki kiss.")]
-        public async Task Kiss(CommandContext ctx, [Description("Wzmianka")] DiscordMember member = null)
-        {
-            await ctx.TriggerTypingAsync();
-            if (ctx.Channel.IsNSFW)
-                await SendImage(ctx, NekosLifePicturesEndpoints.Kiss, "Kiss",  member);
-            else
-                await ctx.RespondAsync("Nie tutaj ;)");
-        }
-    }
+//    [CommandsGroup("Obrazki")]
+//    public class KissCommand : NekosLifeImage
+//    {
+//        [Command("kiss")]
+//        [Description("Wyświetla obrazki kiss.")]
+//        public async Task Kiss(CommandContext ctx, [Description("Wzmianka")] DiscordMember member = null)
+//        {
+//            await ctx.TriggerTypingAsync();
+//            if (ctx.Channel.IsNSFW)
+//                await SendImage(ctx, NekosLifePicturesEndpoints.Kiss, "Kiss",  member);
+//            else
+//                await ctx.RespondAsync("Nie tutaj ;)");
+//        }
+//    }
 }
