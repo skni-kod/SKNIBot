ad94a2d1f880b8bb9039a58b0a58ff2d91a70ae6
AzuxDario <pawel_mibor95@tlen.pl>
2019-02-19 20:07:09 +0100

    Change assign role system

---
+++ SKNIBot/Core/Database/Models/AssignRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SKNIBot.Core.Database.Models
+{
+    public class AssignRole
+    {
+        public AssignRole() { }
+        public AssignRole(ulong id)
+        {
+            RoleID = id.ToString();
+        }
+        public int ID { get; set; }
+        public string RoleID { get; set; }
+
+        public int ServerID { get; set; }
+        public virtual Server Server { get; set; }
+    }
+}
