02f1f0b1967480a97221457aceac18decf3ae1dc
tearth <tearthdev@gmail.com>
2020-03-13 18:15:53 +0100

    Add models for covid

---
+++ SKNIBot/Core/Containers/TextContainers/Covid19/Covid19Container.cs
@@ -0,0 +1,9 @@
+﻿namespace SKNIBot.Core.Containers.TextContainers.Covid19
+{
+    public class Covid19Container
+    {
+        public Covid19Stats Confirmed { get; set; }
+        public Covid19Stats Deaths { get; set; }
+        public Covid19Stats Recovered { get; set; }
+    }
+}
---
+++ SKNIBot/Core/Containers/TextContainers/Covid19/Covid19Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SKNIBot.Core.Containers.TextContainers.Covid19
+{
+    public class Covid19Location
+    {
+        public Covid19Coordinates Coordinates { get; set; }
+        public string Country { get; set; }
+
+        [JsonProperty("country_code")]
+        public string CountryCode { get; set; }
+
+        public Dictionary<DateTime, int> History { get; set; }
+        public int Latest { get; set; }
+        public string Province { get; set; }
+    }
+}
