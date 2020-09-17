333db0ec81197e2f6e9b57c974e7c78e793810df
AzuxDario <pawel_mibor95@tlen.pl>
2020-09-17 11:11:05 +0200

    Added PostLongMessageHelper

---
+++ SKNIBot/Core/Helpers/PostLongMessageHelper.cs
@@ -0,0 +1,36 @@
+﻿using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKNIBot.Core.Helpers
+{
+    class PostLongMessageHelper
+    {
+        public static async Task PostLongMessage(CommandContext ctx, List<string> strings, string header = null)
+        {
+            StringBuilder response = new StringBuilder(2000);
+            foreach (string s in strings)
+            {
+                response.Append(s);
+
+                if (response.Length > 1800)
+                {
+                    await PostEmbedHelper.PostEmbed(ctx, header, response.ToString());
+                    response.Clear();
+                    continue;
+                }
+                if (strings.IndexOf(s) != strings.Count - 1)
+                {
+                    response.Append(", ");
+                }
+            }
+            if (response.Length > 0)
+            {
+                await PostEmbedHelper.PostEmbed(ctx, header, response.ToString());
+                return;
+            }
+        }
+    }
+}
