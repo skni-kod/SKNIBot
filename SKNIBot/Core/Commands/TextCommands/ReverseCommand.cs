6f82d973277143526fd43432df710e745ff9d120
Veuek <73300696+Veuek@users.noreply.github.com>
2021-01-10 22:34:31 +0100

    Update ReverseCommand.cs

--- SKNIBot/Core/Commands/TextCommands/ReverseCommand.cs
+++ SKNIBot/Core/Commands/TextCommands/ReverseCommand.cs
@@ -1,25 +1,30 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
 
-
-namespace SKNIBot.Core.Commands.ManagementCommands
+namespace SKNIBot.Core.Commands.ReverseCommand
 {
     [CommandsGroup("Tekst")]
-    public class PingCommand : BaseCommandModule
+    public class ReverseCommand : BaseCommandModule
     {
-        [Command("odrwoc")]
+        [Command("odwroc")]
         [Description("Odwraca tekst.")]
         public async Task Reverse(CommandContext ctx, [RemainingText] string message = null)
         {
             await ctx.TriggerTypingAsync();
 
-            StringBuilder str = new StringBuilder();
-            for (int i = 0; i < message.Length; i++)
+            if (message == null) await ctx.RespondAsync("Podaj wiadomosc do odwrocenia!");
+            else
             {
-                str.Append(message[message.Length - 1 - i]);
+                StringBuilder str = new StringBuilder();
+                for (int i = 0; i < message.Length; i++)
+                {
+                    str.Append(message[message.Length - 1 - i]);
+                }
+                string mes = str.ToString();
+                await ctx.RespondAsync(mes);
             }
-            string mes = str.toString();
-            await ctx.RespondAsync(mes);
         }
     }
 }
