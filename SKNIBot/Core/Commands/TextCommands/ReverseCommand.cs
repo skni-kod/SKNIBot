b3de40924f385084194479a3e13e316c760b4aba
Veuek <73300696+Veuek@users.noreply.github.com>
2021-01-10 21:47:01 +0100

    Create ReverseCommand.cs

---
+++ SKNIBot/Core/Commands/TextCommands/ReverseCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+
+namespace SKNIBot.Core.Commands.ManagementCommands
+{
+    [CommandsGroup("Tekst")]
+    public class PingCommand : BaseCommandModule
+    {
+        [Command("odrwoc")]
+        [Description("Odwraca tekst.")]
+        public async Task Reverse(CommandContext ctx, [RemainingText] string message = null)
+        {
+            await ctx.TriggerTypingAsync();
+
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < message.Length; i++)
+            {
+                str.Append(message[message.Length - 1 - i]);
+            }
+            string mes = str.toString();
+            await ctx.RespondAsync(mes);
+        }
+    }
+}
