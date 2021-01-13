ac27ad0375d4e5f11cafa319eb8dab66c37097ac
AzuxDario <pawel_mibor95@tlen.pl>
2021-01-13 17:37:50 +0100

    Update PostEmbedHelper.cs

--- SKNIBot/Core/Helpers/PostEmbedHelper.cs
+++ SKNIBot/Core/Helpers/PostEmbedHelper.cs
@@ -4,25 +4,62 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace SKNIBot.Core.Helpers
 {
     static class PostEmbedHelper
     {
-        public static async Task PostEmbed(CommandContext ctx, string title = null, string description = null, string imageLink = null, string footerText = null)
+        public static async Task PostEmbed(CommandContext ctx, string title = null, string description = null, string imageLink = null, string footerText = null,
+            string footerThumbnailLink = null, string embedThumbnailLink = null, string color = null)
         {
+            // Discord can't handle links with japanese characters
+            if (imageLink != null)
+            {
+                int pos = imageLink.LastIndexOf("/");
+                string toChange = imageLink.Substring(pos + 1);
+                string changed = HttpUtility.UrlEncode(toChange, Encoding.UTF8);
+                imageLink = imageLink.Remove(pos + 1);
+                imageLink += changed;
+            }
+            if (embedThumbnailLink != null)
+            {
+                int posThumbnail = embedThumbnailLink.LastIndexOf("/");
+                string toChangeThumbnail = embedThumbnailLink.Substring(posThumbnail + 1);
+                string changedThumbnail = HttpUtility.UrlEncode(toChangeThumbnail, Encoding.UTF8);
+                embedThumbnailLink = embedThumbnailLink.Remove(posThumbnail + 1);
+                embedThumbnailLink += changedThumbnail;
+            }
+
+
+
             var embed = new DiscordEmbedBuilder
             {
-                Color = new DiscordColor("#5588EE"),
                 ImageUrl = imageLink,
                 Description = description,
                 Title = title
             };
-            if (footerText != null)
+
+            if (footerText != null || footerThumbnailLink != null)
             {
-                embed.Footer = new DiscordEmbedBuilder.EmbedFooter();
-                embed.Footer.Text = footerText;
+                embed.Footer = new DiscordEmbedBuilder.EmbedFooter { Text = footerText, IconUrl = footerThumbnailLink };
             }
+
+            if (color == null)
+            {
+                embed.Color = new DiscordColor("#5588EE");
+            }
+            else
+            {
+                embed.Color = new DiscordColor(color);
+            }
+
+            if (embedThumbnailLink != null)
+            {
+                embed.Thumbnail = new DiscordEmbedBuilder.EmbedThumbnail();
+                embed.Thumbnail.Url = embedThumbnailLink;
+            }
+
             await ctx.RespondAsync(null, false, embed);
         }
     }
