f9dc9be8d454d3616807298bf2c454c5ae1ab573
AzuxDario <pawel_mibor95@tlen.pl>
2021-08-25 16:43:27 +0200

    Moved simple message responses to database

---
+++ SKNIBot/Core/Database/Models/StaticDB/MessageResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SKNIBot.Core.Database.Models.StaticDB
+{
+    public class MessageResponse
+    {
+        public int ID { get; set; }
+        public string Message { get; set; }
+        public string Response { get; set; }
+    }
+}
---
+++ SKNIBot/Core/Services/MessageResponseService/MessageResponseService.cs
@@ -0,0 +1,43 @@
+﻿using SKNIBot.Core.Database;
+using SKNIBot.Core.Database.Models.StaticDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SKNIBot.Core.Services.MessageResponseService
+{
+    class MessageResponseService
+    {
+        private Dictionary<string, string> MessageResponses;
+
+        public MessageResponseService()
+        {
+            MessageResponses = new Dictionary<string, string>();
+
+            using (var databaseContext = new StaticDBContext())
+            {
+                List<MessageResponse> responses = databaseContext.MessageResponses.ToList();
+                foreach(var response in responses)
+                {
+                    MessageResponses.Add(response.Message, response.Response);
+                }
+            }
+        }
+
+        public List<string> GetResponses(string message)
+        {
+            List<string> responses = new List<string>();
+
+            foreach(var response in MessageResponses)
+            {
+                if(message.Contains(response.Key, StringComparison.OrdinalIgnoreCase))
+                {
+                    responses.Add(response.Value);
+                }
+            }
+
+            return responses;
+        }
+    }
+}
