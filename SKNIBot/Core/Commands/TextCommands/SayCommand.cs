b8b22a9e160be055b7d534bbb3acb4738cb7c0a6
AzuxDario <pawel_mibor95@tlen.pl>
2021-11-06 10:38:43 +0100

    Update SayCommand.cs

--- SKNIBot/Core/Commands/TextCommands/SayCommand.cs
+++ SKNIBot/Core/Commands/TextCommands/SayCommand.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using SKNIBot.Core.Helpers;
 
 namespace SKNIBot.Core.Commands.TextCommands
 {
@@ -10,20 +11,20 @@ namespace SKNIBot.Core.Commands.TextCommands
         [Command("mów")]
         [Description("Każ mi coś powiedzieć!")]
         [Aliases("mow", "powiedz", "say")]
-        public async Task Say(CommandContext ctx, [Description("Co chcesz powiedzieć?")] string message)
+        public async Task Say(CommandContext ctx, [Description("Co chcesz powiedzieć?")] [RemainingText] string message)
         {
             await ctx.TriggerTypingAsync();
-            await ctx.RespondAsync(message);
+            await PostEmbedHelper.PostEmbed(ctx, "Bocik przemówił", message);
         }
 
         [Command("mówd")]
         [Description("Każ mi coś powiedzieć!")]
         [Aliases("mowd", "powiedzd", "sayd")]
-        public async Task SayD(CommandContext ctx, [Description("Co chcesz powiedzieć?")] string message)
+        public async Task SayD(CommandContext ctx, [Description("Co chcesz powiedzieć?")] [RemainingText] string message)
         {
             await ctx.TriggerTypingAsync();
             await ctx.Channel.DeleteMessageAsync(ctx.Message);
-            await ctx.RespondAsync(message);
+            await PostEmbedHelper.PostEmbed(ctx, "Bocik przemówił", message);
         }
     }
 }
