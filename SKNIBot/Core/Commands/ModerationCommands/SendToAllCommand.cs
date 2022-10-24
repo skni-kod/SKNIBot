7d3d3a80c3291e80f7fb66b6f03b8b6ee8a8c016
AzuxDario <pawel_mibor95@tlen.pl>
2022-10-24 17:59:40 +0200

    Update SendToAllCommand.cs

--- SKNIBot/Core/Commands/ModerationCommands/SendToAllCommand.cs
+++ SKNIBot/Core/Commands/ModerationCommands/SendToAllCommand.cs
@@ -20,19 +20,19 @@ namespace SKNIBot.Core.Commands.ModerationCommands
         public async Task SendToAll(CommandContext ctx, [Description("Rola do której mają zostać wysłane wiadomości.")] DiscordRole role, [RemainingText, Description("Treść wiadomości.")] string message)
         {
             await ctx.TriggerTypingAsync();
-            await ctx.RespondAsync("To mała wiadomość dla ciebie, ale stos wiadomości dla mnie");
-            await ctx.RespondAsync("Apokalipsa zaczyna się");
-            await ctx.RespondAsync("Wyszukiwanie ofiar");
+            await ctx.Channel.SendMessageAsync("To mała wiadomość dla ciebie, ale stos wiadomości dla mnie");
+            await ctx.Channel.SendMessageAsync("Apokalipsa zaczyna się");
+            await ctx.Channel.SendMessageAsync("Wyszukiwanie ofiar");
 
             var members = ctx.Guild.Members.Where(m => !m.Value.IsBot && m.Value.Roles.Contains(role));
 
-            await ctx.RespondAsync($"Znaleziono {members.Count()} ofiar");
+            await ctx.Channel.SendMessageAsync($"Znaleziono {members.Count()} ofiar");
 
-            await ctx.RespondAsync("Wysyłanie orędzia");
+            await ctx.Channel.SendMessageAsync("Wysyłanie orędzia");
 
             var sentMessagesCount = 0;
 
-            DiscordMessage progressBarMessage = await ctx.RespondAsync("`----------" + " 0 %`");
+            DiscordMessage progressBarMessage = await ctx.Channel.SendMessageAsync("`----------" + " 0 %`");
             foreach (var member in members)
             {
                 try
@@ -64,7 +64,7 @@ namespace SKNIBot.Core.Commands.ModerationCommands
                 }
                 catch (Exception ex)
                 {
-                    await ctx.RespondAsync($"Nie udało się wysłać orędzia do {member.Value.DisplayName} ({member.Value.Username}#{member.Value.Discriminator}): {ex}");
+                    await ctx.Channel.SendMessageAsync($"Nie udało się wysłać orędzia do {member.Value.DisplayName} ({member.Value.Username}#{member.Value.Discriminator})");
                 }
             }
 
@@ -74,7 +74,7 @@ namespace SKNIBot.Core.Commands.ModerationCommands
             }
             catch { }
 
-            await ctx.RespondAsync($"Zrobiłem swoje, teraz ty mierz się z gniewem ludu. Wysłano {sentMessagesCount} wiadomości.");
+            await ctx.Channel.SendMessageAsync($"Zrobiłem swoje, teraz ty mierz się z gniewem ludu. Wysłano {sentMessagesCount} wiadomości.");
         }
 
         private string CreateProgressBar(int numberOfRecivers, int value)
